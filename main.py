85f827bccef3fbdb443353e35a05fa5c5a874e95
Shaqalito <ferriisaie@gmail.com>
2021-11-15 20:26:08 +0100

    1.2.6

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ async def fetch_poll_options(guild_id):  # Fetches all currently stored polls to
     options = []  # Create an empty list to store all the options (options will be the polls that can be ended)
     for key in polls[str(guild_id)].keys():  # For each key in the poll's json object
         msg_id = key  # We get the message ID which is the key
-        poll = polls[key]  # We get the poll dictionary
+        poll = polls[str(guild_id)][key]  # We get the poll dictionary
         title = poll["title"]  # And the title of the poll
         options.append(SelectOption(label=title, value=msg_id))  # We then create and append a SelectOption that contains the title of the poll
 
